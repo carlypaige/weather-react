e253fc7cc5235b436e51920a042f99551ca9a9c9
Carly Paige <carly.paige@gmail.com>
2022-09-21 12:55:43 -0500

    consolidated two index pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
+import "./styles.css";
+import Form from "./Form";
+import Overview from "./Overview";
+import Fiveday from "./fiveDay";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <Form />
+        </div>
+        <div>
+          <Overview />
+        </div>
+        <div className="row">
+          <Fiveday />
+        </div>
+        <a
+          href="https://github.com/carlypaige/SheCodes-git-carly"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Coded by Carly Paige
+        </a>
+      </div>
+    </div>
   </React.StrictMode>
 );
 
