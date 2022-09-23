f8ce05f69bff681878520f0f1ab0c03cc3aebf44
Carly Paige <carly.paige@gmail.com>
2022-09-23 18:40:43 -0500

    Reworked components one more time

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 
-export default function Weather(props) {
+export default function Weather() {
   let [weatherData, setWeatherData] = useState({});
   let [ready, setReady] = useState(false);
 
@@ -14,7 +14,6 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
     });
-    console.log(response.data);
     setReady(true);
   }
   if (ready) {
@@ -28,46 +27,7 @@ export default function Weather(props) {
           />
           <input type="submit" value="Search" className="btn btn-danger ms-2" />
         </form>
-        <div className="row">
-          <div className="overview col-6">
-            <h1 className="city ps-2">{weatherData.city}</h1>
-            <div className="icon">
-              <img
-                src="http://openweathermap.org/img/wn/10d@2x.png"
-                alt="Weather Icon"
-              />
-              <span className="current-temp">
-                {Math.round(weatherData.temperature)}°C
-              </span>
-            </div>
-            <div className="current-time ps-2">
-              <FormattedDate date={weatherData.date} />
-            </div>
-            <ul className="current-wh ps-2">
-              <li>wind: {Math.round(weatherData.wind)} km/h</li>
-              <li>humidity: {weatherData.humidity} %</li>
-            </ul>
-          </div>
-          <div className="col-6 five-day">
-            <ul className="five-day-items">
-              <li className="list-group-item">
-                T<span class="low-high"> 57/76 </span>⛅
-              </li>
-              <li className="list-group-item">
-                W<span className="low-high"> 58/71 </span>⛅
-              </li>
-              <li className="list-group-item">
-                R<span className="low-high"> 58/74 </span>🌧️
-              </li>
-              <li className="list-group-item">
-                F<span className="low-high"> 54/75 </span>☀️
-              </li>
-              <li className="list-group-item">
-                S<span className="low-high"> 57/78 </span>☀️
-              </li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
---
+++ src/WeatherInfo.js
@@ -0,0 +1,47 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo row">
+      <div className="overview col-6">
+        <h1 className="city ps-2">{props.data.city}</h1>
+        <div className="icon">
+          <img
+            src="http://openweathermap.org/img/wn/10d@2x.png"
+            alt="Weather Icon"
+          />
+          <span className="current-temp">
+            {Math.round(props.data.temperature)}°C
+          </span>
+        </div>
+        <div className="current-time ps-2">
+          <FormattedDate date={props.data.date} />
+        </div>
+        <ul className="current-wh ps-2">
+          <li>wind: {Math.round(props.data.wind)} km/h</li>
+          <li>humidity: {props.data.humidity} %</li>
+        </ul>
+      </div>
+      <div className="col-6 five-day">
+        <ul className="five-day-items">
+          <li className="list-group-item">
+            T<span class="low-high"> 57/76 </span>⛅
+          </li>
+          <li className="list-group-item">
+            W<span className="low-high"> 58/71 </span>⛅
+          </li>
+          <li className="list-group-item">
+            R<span className="low-high"> 58/74 </span>🌧️
+          </li>
+          <li className="list-group-item">
+            F<span className="low-high"> 54/75 </span>☀️
+          </li>
+          <li className="list-group-item">
+            S<span className="low-high"> 57/78 </span>☀️
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
